72b9e609809524cb4f66c07c0ecf5331980982ab
shariar khan <kshariare@outlook.com>
2024-06-16 18:32:58 +0600

    category wise news showing is added. The Project is done for now. Some minor Ui enhancement is needed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
 // import NewsItem from './components/NewsItem';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   
@@ -12,10 +12,18 @@ export default class App extends Component {
     return (
       <div>
         <Router>
-          <Routes>
-        <NavBar/>
-        <News pageSize={6} country="us" category="sports"/>
+         
+        <NavBar/>   
+        <Routes> 
+        <Route path="/" element={<News key="general" pageSize={6} country="us" category="general" />} />
+        <Route path="/business" element={<News key="business" pageSize={6} country="us" category="business" />} />
+        <Route path="/entertainment" element={<News key="entertainment" pageSize={6} country="us" category="entertainment" />} />
+        <Route path="/health" element={<News key="health" pageSize={6} country="us" category="health" />} />
+        <Route path="/science" element={<News key="science" pageSize={6} country="us" category="science" />} />
+        <Route path="/sports" element={<News key="sports" pageSize={6} country="us" category="sports" />} />
+        <Route path="/technology" element={<News key="technology" pageSize={6} country="us" category="technology" />} />
         </Routes>
+       
         </Router>
       </div>
     )
